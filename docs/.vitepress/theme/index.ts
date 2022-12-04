fee62dcf88ab09057b451f6f7b8b1e700d0f86eb
YQisme <1398757912@qq.com>
2022-12-04 16:01:57 +0800

    添加element-plus UI 库

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,19 @@
 import DefaultTheme from "vitepress/theme";
 import Particles from "vue3-particles";
 
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
     app.use(Particles);
+    app.use(ElementPlus);
+    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+      app.component(key, component);
+    }
   },
 };
+
+// const app = createApp(App)
