2329cc59cd5cc8d14a6ee99b8162b034d499ae17
YQisme <1398757912@qq.com>
2022-12-19 22:27:27 +0800

    对话框UI

---
+++ docs/.vitepress/theme/components/Dialog.vue
@@ -0,0 +1,18 @@
+<template>
+    <el-button class="button" @click="dialogVisible = true">
+        <font color='LightSkyBlue'><el-icon>
+                <DocumentCopy />
+            </el-icon>Bib</font>
+    </el-button>
+
+
+    <el-dialog v-model="dialogVisible" title="BibTex" width="30%" draggable>
+        <span>It's a draggable Dialog</span>
+    </el-dialog>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const dialogVisible = ref(false)
+</script>
