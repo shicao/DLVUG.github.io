5b9db13caf1b86eebed5a7c15808bae6231a4a11
YQisme <1398757912@qq.com>
2022-12-29 23:38:37 +0800

    添加预览图片的详细介绍以及xml文件

--- docs/.vitepress/theme/components/ZouMaDeng.vue
+++ docs/.vitepress/theme/components/ZouMaDeng.vue
@@ -1,12 +1,19 @@
 <script lang="ts" setup>
 const fits = ['fill', 'contain', 'cover', 'none', 'scale-down']
-const urls = ["/imgs/lunbotu/ruangu.jpg", "/imgs/lunbotu/1.jpg", "/imgs/lunbotu/2.jpg", "/imgs/lunbotu/3.jpg", "/imgs/lunbotu/4.jpg", "/imgs/lunbotu/5.jpg"]
+const urls = ["/imgs/lunbotu/document.jpg", "/imgs/lunbotu/ruangu.jpg", "/imgs/lunbotu/1.jpg", "/imgs/lunbotu/2.jpg", "/imgs/lunbotu/3.jpg", "/imgs/lunbotu/4.jpg", "/imgs/lunbotu/5.jpg"]
+const hrefs = ['document', 'ruangu', 'cover', 'none', 'scale-down']
 </script>
 <!-- https://element-plus.gitee.io/zh-CN/component/carousel.html -->
 <template>
     <el-carousel :interval="2000" type="card" height="400px">
-        <el-carousel-item v-for="url in urls" :key="url">
-            <el-image :src="url" :initial-index="4" fit="cover" />
+        <el-carousel-item v-for="(url, index) in urls" :key="index">
+            <a :href=hrefs[index] target="-blank" title="查看详情">
+
+                <el-image style="width: 760px; height: 400px" :src="url" fit="fill" />
+
+
+            </a>
+            <!-- 走马灯的每个子项都是940*400 -->
         </el-carousel-item>
     </el-carousel>
 </template>
